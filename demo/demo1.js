c18ad66e4912a824e9147785e8a0224fb5391833
kinsliy <kinsliy@qq.com>
2017-12-19 14:36:39 +0800

    init

---
+++ demo/demo1.js
@@ -0,0 +1,19 @@
+
+var str ='hello world';
+
+var n = str.search('world');
+
+document.write(n+'   ')
+
+
+var txt=str.replace('world','kinsliy');
+
+document.write(txt+'  ')
+
+var patt ='hello ekinsekre';
+
+var pat=/e/g;
+
+document.write(pat.exec(patt))
+
+
